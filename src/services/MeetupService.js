02c328b184de008c87942d7790e81c8cde06fbad
Diego Avila <Diegoavilap@users.noreply.github.com>
2019-08-22 01:01:29 -0500

    Fixing URL for Api Meetup V2 deprecated (#14)

--- src/services/MeetupService.js
+++ src/services/MeetupService.js
@@ -5,17 +5,17 @@ const baseURL = process.env.VUE_APP_MEETUP_URL
 export default {
   getUpcomingEvents(page) {
     return fetchJsonp(
-      `${baseURL}/events?desc=true&photo-host=public&page=${page}&sig_id=223811643&status=upcoming&sig=88be441521b1fab5ab1330461aa7a9a0101f86b5`
+      `${baseURL}/events?&sign=true&photo-host=public&page=${page}&desc=true&status=upcoming`
     ).then(response => response.json())
   },
   getPreviousEvents(page) {
     return fetchJsonp(
-      `${baseURL}/events?desc=true&photo-host=public&page=${page}&sig_id=223811643&status=past&sig=92a8e04546d333cb9eef865344831eb1a284ee47`
+      `${baseURL}/events?&sign=true&photo-host=public&page=${page}&desc=true&status=past`
     ).then(response => response.json())
   },
   getOrganizers(page) {
     return fetchJsonp(
-      `${baseURL}/members?photo-host=public&role=leads&page=${page}&sig_id=223811643&sig=292d4f45b3d63e0d3f2bf0718f6f50c89a1e7f26`
+      `${baseURL}/members?&sign=true&photo-host=public&role=leads&page=${page}`
     ).then(response => response.json())
   }
 }
